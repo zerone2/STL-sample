0ab21abdd76a4440dcf1d67d73f2906f7fbefacb
zerone2 <yicho93@naver.com>
2020-08-29 22:52:20 +0900

    [add] build

---
+++ build/precache-manifest.d7c8a565fcb62c0f022955bd4301c8fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67d835b91c268c734e86b639b3d43e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d8112a17eb5243bdfa0",
+    "url": "/static/css/main.b7a5d657.chunk.css"
+  },
+  {
+    "revision": "75e03e21228a1d86467f",
+    "url": "/static/js/2.04e486b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04e486b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8112a17eb5243bdfa0",
+    "url": "/static/js/main.90ff9f4a.chunk.js"
+  },
+  {
+    "revision": "2fb286579665648ae9e6",
+    "url": "/static/js/runtime-main.c8be038c.js"
+  }
+]);
